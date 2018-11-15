f5447f801d4881ccc110147900fc235167fab75b
toktarbaev <43265019+toktarbaev@users.noreply.github.com>
2018-11-15 13:11:07 +0600

    Add files via upload

---
+++ Lab5/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Operations opp = new Operations();
+        Writer writer = new Writer();
+        writer.write(opp.Do());
+    }
+
+}
